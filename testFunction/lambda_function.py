85d749b1f713dd5beaabd0e0bfc65fd1e806f2a5
TConnellan <44292007+TConnellan@users.noreply.github.com>
2025-04-14 19:52:15 +1000

    Update lambda_function.py

--- testFunction/lambda_function.py
+++ testFunction/lambda_function.py
@@ -2,6 +2,6 @@ import json
 
 def lambda_handler(event, context):
     return {
-        'statusCode': 201,
+        'statusCode': 200,
         'body': json.dumps('Modified and deployed via ci/cd!')
     }
